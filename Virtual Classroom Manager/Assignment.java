0d227de10059fca13ded0d2e908fc1629f1132a4
anandbabu1805 <125114317+anandbabu1805@users.noreply.github.com>
2024-07-28 23:51:16 +0530

    Update Assignment.java

--- Virtual Classroom Manager/Assignment.java
+++ Virtual Classroom Manager/Assignment.java
@@ -1,11 +1,23 @@
+import java.time.LocalDate;
+
 public class Assignment {
     private String details;
+    private LocalDate deadline;
 
-    public Assignment(String details) {
+    public Assignment(String details, LocalDate deadline) {
         this.details = details;
+        this.deadline = deadline;
     }
 
     public String getDetails() {
         return details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public boolean isDeadlineCrossed() {
+        return LocalDate.now().isAfter(deadline);
     }
 }
