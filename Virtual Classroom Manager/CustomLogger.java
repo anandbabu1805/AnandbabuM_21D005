934bb7d8eb81d657d0fa87fc9e46a3df46b6a663
anandbabu1805 <125114317+anandbabu1805@users.noreply.github.com>
2024-07-28 20:54:55 +0530

    Create CustomLogger.java

---
+++ Virtual Classroom Manager/CustomLogger.java
@@ -0,0 +1,14 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CustomLogger {
+    private static final Logger logger = Logger.getLogger(CustomLogger.class.getName());
+
+    public static void logInfo(String message) {
+        logger.log(Level.INFO, message);
+    }
+
+    public static void logError(String message, Exception e) {
+        logger.log(Level.SEVERE, message, e);
+    }
+}
