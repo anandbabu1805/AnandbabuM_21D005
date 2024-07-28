af3073bb1ea76f865922e1490d783290ef8eb0a7
anandbabu1805 <125114317+anandbabu1805@users.noreply.github.com>
2024-07-28 20:55:59 +0530

    Create ListStudentsCommand.java

---
+++ Virtual Classroom Manager/ListStudentsCommand.java
@@ -0,0 +1,14 @@
+public class ListStudentsCommand implements Command {
+    private VirtualClassroomManager manager;
+    private String className;
+
+    public ListStudentsCommand(VirtualClassroomManager manager, String className) {
+        this.manager = manager;
+        this.className = className;
+    }
+
+    @Override
+    public void execute() {
+        manager.listStudents(className);
+    }
+}
